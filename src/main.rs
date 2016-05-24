d2b1d054b73f5ea97141d1ef2e6edbbd4197013e
Vasily Kirichenko <vaskir@gmail.com>
2016-05-24 22:09:49 +0300

    basichll

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+extern crate basichll;
+extern crate uuid;
+extern crate time;
+
+#[macro_use] 
+mod utils;
+ 
+use basichll as hll;
+use uuid::Uuid;
+
+fn main() {
+    let mut hll = hll::HLL::new(0.01625);
+    let keys = (1..1000).map(|_| Uuid::new_v4().to_string()).collect::<Vec<_>>();
+    let keys = keys.into_iter().cycle().take(50_000_000).collect::<Vec<_>>();
+    
+    let count = time!("total", {
+        for k in keys {
+            hll.insert(&k);
+        }
+    
+        hll.count().round()
+    });
+    
+    println!("hll.len().round() = {}", count);
+    println!("Done.");
+}
