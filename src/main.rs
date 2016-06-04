888b9275cec8e88e958f14aa78e82aebab155ce7
Vasily Kirichenko <vaskir@gmail.com>
2016-06-04 12:50:40 +0300

    formatting

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,15 @@ use hyperloglog as hll;
 use uuid::Uuid;
 
 fn main() {
-    let mut hll = hll::HyperLogLog::new(0.01625);
-    let keys = (1..1000).map(|_| Uuid::new_v4().to_string()).collect::<Vec<_>>();
-    let keys = keys.into_iter().cycle().take(50_000_000).collect::<Vec<_>>();
+    let mut hll = hll::HyperLogLog::new(0.001);
+    let keys = 
+        (1..1000)
+            .map(|_| Uuid::new_v4().to_string())
+            .collect::<Vec<_>>()
+            .into_iter()
+            .cycle()
+            .take(50_000_000)
+            .collect::<Vec<_>>();
     
     let count = time!("total", {
         for k in keys {
